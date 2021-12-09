d4f909763dfd80a77b9035568c87e0e88c43a61a
msosic97 <msosic97@gmail.com>
2021-12-09 03:09:10 +0100

    add cors url

--- shrb/settings.py
+++ shrb/settings.py
@@ -163,7 +163,7 @@ django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://cranky-heisenberg-878d76.netlify.app",
+    "kind-wozniak-cfcd11.netlify.app",
 ]
 
 CORS_ALLOW_METHODS = [
