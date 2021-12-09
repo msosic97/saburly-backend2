381a68f2783f06bd5b3612fe8d065bb6583aa0f4
msosic97 <msosic97@gmail.com>
2021-12-09 03:20:06 +0100

    changed url for netlify

--- shrb/settings.py
+++ shrb/settings.py
@@ -163,7 +163,7 @@ django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "kind-wozniak-cfcd11.netlify.app",
+    "https://shrb2.herokuapp.com/",
 ]
 
 CORS_ALLOW_METHODS = [
